b39b79522193c61adf3fa788a274f1dad6f2521b
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-05 21:55:52 +0600

    Use fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import fastifyMulipart from 'fastify-multipart';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.register(fastifyMulipart);
+
   await app.listen(3000);
 }
+
 bootstrap();
