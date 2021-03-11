a2801bf0b8686c5d6096cf1deeb6e295a2773a1d
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-11 20:49:39 +0600

    Import AuthModule, UserModule and MongooseModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
-import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { SpeechRecognitionModule } from './speech-recognition/speech-recognition.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SpeechRecognitionModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const databaseUri = configService.get<string>('DATABASE_URI');
+
+        return {
+          uri: databaseUri,
+          useCreateIndex: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+    SpeechRecognitionModule,
+  ],
 })
 export class AppModule {}
