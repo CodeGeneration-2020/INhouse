77200473fc9e65415e1052ea95aad6b93f4b553d
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-15 14:25:44 +0200

    add custom useform hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
-import './App.css';
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import './App.scss';
+import { BrowserRouter, Route } from "react-router-dom";
 import Auth from './components/Auth/Auth';
 import UserInfo from './components/UserInfo/UserInfo';
+import ProtectedRoute from './helpers/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" render={() => (
-          localStorage.getItem('token') ?
-            (<Redirect to="/info" />)
-            :
-            (<Redirect to="/login" />)
-        )} />
-        <Route exact path="/info" component={UserInfo} />
+        <Route exact path="/" component={() => <ProtectedRoute Component={UserInfo} />} />
         <Route exact path="/register" component={() => <Auth auth={'register'} />} />
         <Route exact path="/login" component={() => <Auth auth={'login'} />} />
       </BrowserRouter>
