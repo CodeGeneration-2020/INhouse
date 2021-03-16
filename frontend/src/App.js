b5d80edb989ea8bd9482f8267b0d13df14af013a
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-16 11:23:07 +0200

    create cards, rename user to userContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
 import { BrowserRouter, Route } from "react-router-dom";
 import Auth from './components/Auth/Auth';
-import User from './components/User/User';
+import UserContainer from './components/UserContainer/User.Container';
 import ProtectedRoute from './helpers/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={() => <ProtectedRoute Component={User} />} />
+        <Route exact path="/" component={() => <ProtectedRoute Component={UserContainer} />} />
         <Route exact path="/register" component={() => <Auth auth={'register'} />} />
         <Route exact path="/login" component={() => <Auth auth={'login'} />} />
       </BrowserRouter>
