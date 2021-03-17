87a20df8e6062965d25a6d253b3416a0bf29209a
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-17 19:12:02 +0200

    implement linkedin info view

--- frontend/src/components/UserContainer/Humantic/Humantic.js
+++ frontend/src/components/UserContainer/Humantic/Humantic.js
@@ -1,31 +1,53 @@
 import { Button, Input } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from 'react-query';
 import { useForm } from '../../../helpers/Hooks/UseForm';
-import { HUMANTIC } from '../constants/constants';
 import classes from './Humantic.module.scss';
 import { BUTTONS } from '../../../helpers/constants/constants';
 import userService from '../../../services/userService';
 
 const Humantic = () => {
+  const [linkedinInfo, setLinkedinInfo] = useState()
+
   const [inputValue, setField] = useForm({ linkedInUrl: '' })
   const mutationHumantic = useMutation(() => userService.createHumantic(inputValue.linkedInUrl), {
-    onSuccess: res => console.log(res)
+    onSuccess: res => {
+      setLinkedinInfo(res)
+      console.log(res);
+    }
   })
 
   const sendLinkedin = () => mutationHumantic.mutate()
 
   return (
     <div className={classes.humantic}>
-      <Input
-        placeholder='linkedIn URL'
-        name="linkedInUrl"
-        value={inputValue.linkedInUrl}
-        onChange={setField} />
-      <Button variant="contained" color="primary" onClick={sendLinkedin}>
-        {BUTTONS.send}
-      </Button>
-      <h1>{HUMANTIC.title}</h1>
+      <div>
+        <Input
+          placeholder='linkedIn URL'
+          name="linkedInUrl"
+          value={inputValue.linkedInUrl}
+          onChange={setField} />
+        <Button variant="contained" color="primary" onClick={sendLinkedin}>
+          {BUTTONS.send}
+        </Button>
+      </div>
+      {linkedinInfo &&
+        <div className={classes.linkedinInfo}>
+          <h3>{linkedinInfo.display_name}</h3>
+          <p style={{color: 'blue'}}>
+            <b>Adjectives:</b> {linkedinInfo.persona.hiring.communication_advice.adjectives.join(', ')}.
+          </p>
+          <p>
+            <b>Description:</b> {linkedinInfo.persona.hiring.communication_advice.description.join(' ')}
+          </p>
+          <p style={{color: 'red'}}>
+            <b>What to avoid:</b> {linkedinInfo.persona.hiring.communication_advice.what_to_avoid.join('. ')}
+          </p>
+          <p>
+            <b>What to say:</b> {linkedinInfo.persona.hiring.communication_advice.what_to_say.join('. ')}
+          </p>
+        </div>
+      }
     </div>
   )
 }
