9f9f8a74dfff860459306b17f2c640bcef6eabc2
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-23 18:49:12 +0200

    send record on stop

--- frontend/src/components/UserContainer/Recognition/Recocnition.js
+++ frontend/src/components/UserContainer/Recognition/Recocnition.js
@@ -9,7 +9,7 @@ import RecognitionRow from './RecognitionRow/RecognitionRow';
 
 const Recognition = () => {
   const [record, setRecord] = useState(false)
-  const [blob, setBlob] = useState(new Blob([' '], { type: 'text/plain' }))
+  const [audio, setAudio] = useState(new Blob([' '], { type: 'text/plain' }))
   const [recognitionRows, setRecognitionRows] = useState([])
 
   const answerMutation = useMutation(question => userService.getAnswer(question), {
@@ -18,15 +18,17 @@ const Recognition = () => {
     )
   })
 
-  const questionMutation = useMutation(() => userService.questionRecognition(blob), {
+  const questionMutation = useMutation(blob => userService.questionRecognition(blob), {
     onSuccess: res => answerMutation.mutate(res.text)
   })
 
-  const recognizeHandler = () => questionMutation.mutate()
 
   const startRecording = () => setRecord(true)
   const stopRecording = () => setRecord(false)
-  const onStop = recordedBlob => setBlob(recordedBlob.blob)
+  const onStop = recordedBlob => {
+    questionMutation.mutate(recordedBlob.blob)
+    setAudio(recordedBlob.blob)
+  }
 
   return (
     <div className={classes.record}>
@@ -34,8 +36,7 @@ const Recognition = () => {
         <ReactMic backgroundColor="white" strokeColor="#000000" record={record} onStop={onStop} />
         <Button variant="contained" color="primary" onClick={startRecording}>{BUTTONS.start}</Button>
         <Button onClick={stopRecording} variant="contained" color="secondary">{BUTTONS.stop}</Button>
-        <audio controls src={URL.createObjectURL(blob)} />
-        <Button variant="contained" color="primary" onClick={recognizeHandler}>{BUTTONS.send}</Button>
+        <audio controls src={URL.createObjectURL(audio)} />
       </div>
       {recognitionRows.map((recognitionRow, index) =>
         <RecognitionRow key={index} recognitionRow={recognitionRow} />
