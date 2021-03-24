a90a7e759d36a39f08fb4b367f5f30e2adf06847
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-24 19:43:45 +0600

    Create metric for recognized audio

--- backend/src/speech-recognition/speech-recognition.module.ts
+++ backend/src/speech-recognition/speech-recognition.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { MetricModule } from '../metric/metric.module';
+
 import { SpeechRecognitionController } from './speech-recognition.controller';
 
 import { SpeechRecognitionService } from './speech-recognition.service';
 import { MicrosoftSpeechRecognitionService } from './microsoft-speech-recognition.service';
 
 @Module({
+  imports: [MetricModule],
   controllers: [SpeechRecognitionController],
   providers: [
     {
