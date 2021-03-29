264200b7cf763a069b984980282e8c766b2ffb1e
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-29 15:42:12 +0600

    Create file module

---
+++ backend/src/file/schemas/file.schema.ts
@@ -0,0 +1,41 @@
+import { Readable, Writable } from 'stream';
+
+import * as schema from 'gridfile';
+
+import { Schema, Document } from 'mongoose';
+
+export abstract class File {
+  length: number;
+
+  chunkSize: number;
+
+  uploadDate: Date;
+
+  md5: string;
+
+  filename: string;
+
+  contentType: string;
+
+  metadata: any;
+
+  aliases: string[];
+
+  // Issue: https://github.com/microsoft/TypeScript/issues/34516
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static findByIdAndDelete(id: any): FileDocument {
+    return {} as FileDocument;
+  }
+
+  abstract getUploadStream(): Writable;
+
+  abstract getDownloadStream(): Readable;
+
+  abstract upload(stream: Readable): FileDocument;
+
+  abstract download(stream: Writable): FileDocument;
+}
+
+export type FileDocument = File & Document<File>;
+
+export const FileSchema: Schema<FileDocument> = schema;
