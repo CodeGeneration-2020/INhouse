aebb33213eb80456fffe5ffedd071878dd538ec8
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-03-31 17:22:51 +0600

    Create Auth controller

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 
+import { UserModule } from 'src/user/user.module';
+
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-
-import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { UserModule } from '../user/user.module';
     PassportModule,
     UserModule,
   ],
+  controllers: [AuthController],
   providers: [AuthService, JwtStrategy, LocalStrategy],
   exports: [AuthService],
 })
