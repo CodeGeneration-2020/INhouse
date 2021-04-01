31ad99a9832ba3c4e4b322788e2d428e53c22d44
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-04-01 12:32:53 +0300

    move search styles to folder

--- frontend/src/components/AdminPanelContainer/Search.js
+++ frontend/src/components/AdminPanelContainer/Search.js
@@ -1,33 +1,20 @@
-import { IconButton, InputBase, makeStyles, Paper } from '@material-ui/core';
+import { IconButton, InputBase, Paper } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import ClearIcon from '@material-ui/icons/Clear';
 import React from 'react'
-
-const searchStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    marginLeft: 'auto',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-    marginBottom: '20px'
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-}));
+import { searchStyles } from '../../styles/components/SearchStyles';
 
 const Search = ({ inputEl, searchHandler, clearSearchHandler }) => {
   const classes = searchStyles()
 
   return (
     <Paper className={classes.root}>
-      <InputBase className={classes.input} placeholder="Search User" inputRef={inputEl}/>
-      <IconButton className={classes.iconButton} onClick={searchHandler}><SearchIcon /></IconButton>
+      <InputBase
+        className={classes.input}
+        placeholder="Search User"
+        inputRef={inputEl}
+      />
+      <IconButton onClick={searchHandler}><SearchIcon /></IconButton>
       <IconButton onClick={clearSearchHandler}><ClearIcon /></IconButton>
     </Paper>
   )
