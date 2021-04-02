c89765e938a71c4a19294a85694a2c4182215dd0
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-04-02 13:42:46 +0300

    add 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Auth from './components/Auth/Auth';
 import UserContainer from './components/UserContainer/User.Container';
 import PreProtectedRoute from './helpers/ProtectedRoutes/PreProtectedRoute';
 import AdminProtectedRoute from './helpers/ProtectedRoutes/AdminProtectedRoute';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import AdminPanelContainer from './components/AdminPanelContainer/AdminPanelContainer';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   const queryClient = new QueryClient()
@@ -14,11 +15,14 @@ function App() {
     <div className="App">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Route exact path="/" component={() => <PreProtectedRoute Component={UserContainer} />} />
-          <Route exact path="/admin_panel" component={() => <AdminProtectedRoute Component={AdminPanelContainer} />} />
-          <Route exact path="/register" component={() => <Auth authType={'register'} />} />
-          <Route exact path="/login" component={() => <Auth authType={'login'} />} />
-          <Route exact path="/add_user" component={() => <Auth authType={'create'} />} />
+          <Switch>
+            <Route exact path="/" component={() => <PreProtectedRoute Component={UserContainer} />} />
+            <Route exact path="/admin_panel" component={() => <AdminProtectedRoute Component={AdminPanelContainer} />} />
+            <Route exact path="/register" component={() => <Auth authType={'register'} />} />
+            <Route exact path="/login" component={() => <Auth authType={'login'} />} />
+            <Route exact path="/add_user" component={() => <Auth authType={'create'} />} />
+            <Route component={NotFound} />
+          </Switch>
         </BrowserRouter>
       </QueryClientProvider>
     </div>
