c4de3daf7bb7f4b261cc6f11e57ef6cce92143bc
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-04-02 20:41:15 +0600

    Add missed decorator

--- backend/src/dialog/dto/create-one.dto.ts
+++ backend/src/dialog/dto/create-one.dto.ts
@@ -1,10 +1,11 @@
 import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { IsDefined, ValidateNested } from 'class-validator';
 
 import { DialogDto } from './dialog.dto';
 
 export class CreateOneDto {
   @Type(() => DialogDto)
+  @IsDefined()
   @ValidateNested()
   dialog: DialogDto;
 }
