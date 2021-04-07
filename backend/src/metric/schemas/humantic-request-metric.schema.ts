a9722f93051ce863f0cc4d4751dfe5cae5e7ad6e
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-04-07 20:28:41 +0600

    Declare class HumanticRequestMetric with class-transformer

--- backend/src/metric/schemas/humantic-request-metric.schema.ts
+++ backend/src/metric/schemas/humantic-request-metric.schema.ts
@@ -1,32 +1,44 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+import { Type, Expose } from 'class-transformer';
 import { Document, SchemaTypes } from 'mongoose';
+
+import { User } from 'src/user/schemas/user.schema';
+import { ProfileAnalysis } from 'src/humantic-ai/schemas/profile-analysis.schema';
 
 @Schema({
   versionKey: false,
 })
 export class HumanticRequestMetric {
+  @Type(() => User)
   @Prop({
-    type: SchemaTypes.String,
-    index: true,
+    type: SchemaTypes.ObjectId,
+    ref: User.name,
     required: true,
   })
-  userId: string;
+  @Expose()
+  user: User;
 
+  @Type(() => ProfileAnalysis)
   @Prop({
-    type: SchemaTypes.String,
+    type: SchemaTypes.ObjectId,
+    ref: ProfileAnalysis.name,
     required: true,
   })
-  profileAnalysisId: string;
+  @Expose()
+  profileAnalysis: ProfileAnalysis;
 
+  @Type(() => Date)
   @Prop({
     type: SchemaTypes.Date,
     default: () => new Date(),
   })
+  @Expose()
   createdAt: Date;
 }
 
-export type HumanticRequestMetricDocument = HumanticRequestMetric & Document;
+export type HumanticRequestMetricDocument = HumanticRequestMetric &
+  Document<HumanticRequestMetric>;
 
 export const HumanticRequestMetricSchema = SchemaFactory.createForClass(
   HumanticRequestMetric,
