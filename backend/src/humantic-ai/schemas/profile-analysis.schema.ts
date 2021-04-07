00070d8ec6ef119a38974d1435f965c472cd5d68
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-04-07 20:30:27 +0600

    Declare class ProfileAnalysis for class-transformer

--- backend/src/humantic-ai/schemas/profile-analysis.schema.ts
+++ backend/src/humantic-ai/schemas/profile-analysis.schema.ts
@@ -1,26 +1,44 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+import { Type, Expose } from 'class-transformer';
 import { Document, SchemaTypes } from 'mongoose';
+
+import { Role } from 'src/shared/role.enum';
 
 @Schema({
   versionKey: false,
 })
 export class ProfileAnalysis {
+  @Type(() => String)
+  @Expose({
+    name: '_id',
+    groups: [Role.ADMIN, Role.PRE, Role.SDRE],
+  })
+  id: string;
+
+  @Type(() => String)
   @Prop({
     type: SchemaTypes.String,
     index: true,
     unique: true,
     required: true,
   })
+  @Expose({
+    groups: [Role.ADMIN, Role.PRE, Role.SDRE],
+  })
   linkedInUrl: string;
 
   @Prop({
     type: SchemaTypes.Mixed,
   })
+  @Expose({
+    groups: [Role.ADMIN, Role.PRE, Role.SDRE],
+  })
   analysis: any;
 }
 
-export type ProfileAnalysisDocument = ProfileAnalysis & Document;
+export type ProfileAnalysisDocument = ProfileAnalysis &
+  Document<ProfileAnalysis>;
 
 export const ProfileAnalysisSchema = SchemaFactory.createForClass(
   ProfileAnalysis,
